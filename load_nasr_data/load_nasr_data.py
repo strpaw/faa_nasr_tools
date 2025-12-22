ccfb176f95c942444e41e2170037617ad3ea5459
Paweł Strzelewicz <pawel.strzelewicz83@gmail.com>
2025-12-23 00:50:51 +0100

    feat(load_data): add loading CSV files into tables

--- load_nasr_data/load_nasr_data.py
+++ load_nasr_data/load_nasr_data.py
@@ -1,9 +1,11 @@
 """Load NASR data from CSV file into database"""
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
 import pandas as pd
 from pydantic import BaseModel
+import geopandas as gpd
 from sqlalchemy import create_engine, Engine
 from yaml import safe_load
 
@@ -131,6 +133,86 @@ def load_dict_tables(dict_tables: list[DictTableSettings],
                         engine=engine)
 
 
+@dataclass
+class DataTableLoader:
+    """
+    Load data into tables from NASR CSV files based on configuration settings.
+
+    Attributes:
+        data_dir: directory with NASR data CSV files
+        csv_settings: global CSV settings to read NASR data CSV files
+        engine: target database connection
+    """
+
+    data_dir: Path
+    csv_settings: CSVSettings
+    engine: Engine
+
+
+    def _prepare_data(self, data_file_setting: DataFileSettings) -> pd.DataFrame:
+        """Helper method to read data from NASR CSV files.
+
+        :param data_file_setting: file settings for NASR CSV file to be loaded into table
+        :return: dataframe of data
+        """
+        df = pd.read_csv(self.data_dir / data_file_setting.file_name,
+                         delimiter=self.csv_settings.delimiter,
+                         encoding=self.csv_settings.encoding,
+                         quotechar=self.csv_settings.quote_char,
+                         usecols=data_file_setting.columns,)
+        df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
+        df.rename(columns={col: col.lower()
+                           for col in df.columns},
+                  inplace=True)
+        return df
+
+    def _load_non_spatial(self, data: pd.DataFrame,
+                            data_file_setting: DataFileSettings) -> None:
+        """Load NASR CSV file into tables without spatial information.
+
+        :param data: data to be loaded into table
+        :param data_file_setting: file settings for NASR CSV file to be loaded into table
+        """
+        data.to_sql(name=data_file_setting.table_name,
+                    con=self.engine,
+                    if_exists="append",
+                    index=False)
+
+    def _load_spatial(self,
+                        data: pd.DataFrame,
+                        data_file_setting: DataFileSettings) -> None:
+        """Load NASR CSV file into tables with spatial information.
+
+        :param data: data to be loaded into table
+        :param data_file_setting: file settings for NASR CSV file to be loaded into table
+        """
+        gdf = gpd.GeoDataFrame(
+            data=data,
+            geometry=gpd.points_from_xy(data.long_decimal, data.lat_decimal),
+            crs="EPSG:4326"
+        )
+        gdf.drop(columns=["long_decimal", "lat_decimal"], inplace=True)
+        gdf.to_postgis(name=data_file_setting.table_name,
+                       con=self.engine,
+                        if_exists="append",
+                        index=False,
+                        chunksize=10000)
+
+
+    def load_table(self, data_file_setting: DataFileSettings) -> None:
+        """Load NASR CSV file into tables.
+
+        :param data_file_setting: file settings for NASR CSV file to be loaded into table
+        """
+        data = self._prepare_data(data_file_setting)
+        if data_file_setting.is_spatial:
+            self._load_spatial(data=data,
+                               data_file_setting=data_file_setting)
+        else:
+            self._load_non_spatial(data=data,
+                                   data_file_setting=data_file_setting)
+
+
 def main():
     config = load_config(Path("config.yaml"))
     engine = create_engine(
@@ -139,6 +221,11 @@ def main():
 
     load_dict_tables(dict_tables=config.dict_tables,
                      engine=engine)
+
+    data_tables_lookup = {t.file_name : t for t in config.data_tables}
+    dtl = DataTableLoader(data_dir=config.data_dir,
+                          csv_settings=config.csv_settings,
+                          engine=engine)
 
 
 if __name__ == "__main__":
