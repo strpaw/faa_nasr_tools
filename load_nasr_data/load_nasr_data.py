e05c455cf85508a8f1a831132277020e73d93a5b
Paweł Strzelewicz <pawel.strzelewicz83@gmail.com>
2025-12-29 16:03:16 +0100

    feat(load_data): add loading Navigation Aid (NAV) data

--- load_nasr_data/load_nasr_data.py
+++ load_nasr_data/load_nasr_data.py
@@ -231,6 +231,10 @@ def main():
     dtl.load_table(data_file_setting=data_tables_lookup["FIX_CHRT.csv"])
     dtl.load_table(data_file_setting=data_tables_lookup["FIX_NAV.csv"])
 
+    dtl.load_table(data_file_setting=data_tables_lookup["NAV_BASE.csv"])
+    dtl.load_table(data_file_setting=data_tables_lookup["NAV_CKPT.csv"])
+    dtl.load_table(data_file_setting=data_tables_lookup["NAV_RMK.csv"])
+
     dtl.load_table(data_file_setting=data_tables_lookup["RDR.csv"])
 
     dtl.load_table(data_file_setting=data_tables_lookup["WXL_BASE.csv"])
